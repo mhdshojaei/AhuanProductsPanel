cc90ada787bff1998baa996f7c9a3cbbeae1da6f
mahdiShojaei <s.mhd.shojaei@gmail.com>
2025-04-15 21:54:16 +0330

    @fix/fix tsconfig

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './router';
-import 'vuetify/styles';
+import 'vuetify/styles/main.css';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
